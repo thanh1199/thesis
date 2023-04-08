e227344ddb824d5d80112aced389bc7b57e8159f
thanh1199 <quocthanhnguyen07@gmail.com>
2023-04-08 13:03:49 +0900

    20230408

---
+++ src/dbconnect.php
@@ -0,0 +1,26 @@
+﻿<?php
+
+//dbconnect.php ; DB接続
+
+	define('DB_HOST','sv2.php.xdomain.ne.jp');
+	define('DB_NAME','kiisenglish_01');
+	define('DB_USER','kiisenglish_1');
+	define('DB_PASSWORD','binmin49');
+
+   $options = array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET CHARACTER SET 'utf8'");
+   error_reporting(E_ALL & ~E_NOTICE);
+
+	try{
+		$dbh = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME,DB_USER,DB_PASSWORD,$options);
+		$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+	}catch(PDOException $e){
+		echo $e->getMessage();
+		exit;
+	}
+
+	date_default_timezone_set('Asia/Tokyo');
+//	header("Content-Type: text/html; charset=UTF-8");
+//	echo "db connect....<br>";
+?>
+
+
